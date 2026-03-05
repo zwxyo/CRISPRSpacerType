806415698266ede2f911bfa7a1cdb355158786f7
jszwxlll <1109014198@qq.com>
2026-03-05 20:33:23 +0800

    Update CRISPRSpacerType.sh

--- bin/CRISPRSpacerType.sh
+++ bin/CRISPRSpacerType.sh
@@ -22,6 +22,7 @@ CT=false
 MLST=false
 ONLY_CT=false
 PCR_seq=false
+CCF_json=false
 # THREADS=1
 
 
@@ -47,6 +48,7 @@ usage() {
     echo -e "  \033[1;32m--mlst\033[0m          MLST result"
     echo -e "  \033[1;32m--only\033[0m          Only perform CRISPR Typing"
     echo -e "  \033[1;32m--pcr\033[0m           Input pcr seq"
+    echo -e "  \033[1;32m--ccf\033[0m           Input CRISPRCasFinder json file format"
     echo -e "  \033[1;32m-h, --help\033[0m      Show this help message and exit"
     echo -e "\033[1;36m-------------------------------------------------------------\033[0m"
     echo -e "\033[1;34mFor more details, see:\033[0m \033[1;36mhttps://github.com/zwxyo/CRISPRSpacerType\033[0m"
@@ -70,6 +72,7 @@ while [[ $# -gt 0 ]]; do
     --mlst) MLST=true; shift;;
     --only) ONLY_CT=true; shift;;
     --pcr) PCR_seq=true; shift;;
+    --ccf) CCF_json=true; shift;;
 #    --threads)
 #      if [[ "$2" == "auto" ]]; then
 #        THREADS=$(nproc)
@@ -85,7 +88,84 @@ done
 #echo "Using $THREADS threads"
 
 #================================================================================
-if [[ "$PCR_seq" == true && "$CT" == true ]]; then
+# CRISPRCasFinder json
+if [[ "$CCF_json" == true && "$CT" == true && "$ONLY_CT" == true ]]; then
+
+  echo "Running CRISPRCasFinder json file processing..."
+
+  # CRISPR
+  python3 -c "import sys; sys.path.append('$python_module_dir'); from Json_process import batch_extract; batch_extract('${CSI}/ccf_result', '${result_summary}/result.csv')"
+  python3 -c "import sys; sys.path.append('$python_module_dir'); from CRISPR_process import Data_filtering; Data_filtering('${result_summary}/result.csv', '${result_summary}/result_process.csv')"
+
+  # CAS
+  python3 -c "import sys; sys.path.append('$python_module_dir'); from Json_process import batch_extract_cas; batch_extract_cas('${CSI}/ccf_result', '${result_summary}/cas')"
+
+  #--------------------------------------------------------------------------------
+  # BLAST
+  # check parameter BI
+  if [[ "$BT" != "" && "$BT" != "m" && "$BT" != "s" ]]; then
+      echo -e "\033[1;31mError:\033[0m --bt must be 'm' or 's'."
+      exit 1
+  elif [[ "$BT" == "s" && -n "$BI" && "$BI" != "$CSI" ]]; then
+      echo -e "\033[1;31mError:\033[0m When using the standard BLAST type (--bt s), the --bi option must either be omitted or set to the same path as --csi."
+      exit 1
+  elif [[ "$BT" == "m" && -z "$DB" ]]; then
+      echo -e "\033[1;31mError:\033[0m If you opt to utilize a custom BLAST database, please ensure the presence of your custom database FNA file."
+      exit 1
+  elif [[ "$BT" == "m" && ! -f "$DB" ]]; then
+      echo -e "\033[1;31mError:\033[0m Custom BLAST DB file $DB not found!"
+      exit 1
+  elif [[ "$BT" == "s" && -n "$DB" ]]; then
+      echo -e "\033[1;31mError:\033[0m Customization of the BLAST database path is not required."
+      exit 1
+  fi
+
+  if compgen -G "$BI/*.fna" > /dev/null; then
+      if [[ "$BT" == "m" && -n "$DB" ]]; then
+        # python3 BLAAST/BLAST_M.py "$BI"
+        # python3 -c "import sys; sys.path.append('$python_module_dir'); sys.argv = ['BLAST_M.py', '$BI', '$DB', '$result_summary/blast_result']; exec(open('$BLAST_M_script').read())"
+        python3 -c "import sys; sys.path.append('$python_module_dir'); from BLAST_M import BLAST_M; BLAST_M('$BI', '$DB', '${result_summary}/blast_result')"
+
+      elif [[ "$BT" == "s" ]]; then
+        # python3 BLAST/BLAST_S.py "$BI"
+        # python3 -c "import sys; sys.path.append('$python_module_dir'); sys.argv = ['BLAST_S.py', '$BI', '$result_summary/blast_result']; exec(open('$BLAST_S_script').read())"
+        python3 -c "import sys; sys.path.append('$python_module_dir'); from BLAST_S import BLAST_S; BLAST_S('$BI', '${result_summary}/blast_result')"
+
+      fi
+  else
+      echo -e "\033[1;31mError:\033[0m the '.fna' file was not found in directory $BI, skipping the BLAST step..."
+      exit 1
+  fi
+
+  #--------------------------------------------------------------------------------
+  # CRISPR classification
+  # python3 CRISPR_classification.py
+  # python3 -c "import sys; sys.path.append('$python_module_dir'); sys.argv = ['CRISPR_classification.py', '${CTO}/result_process.csv', '$result_summary/cas', '$result_summary/CRISPR_sort', '$result_summary/blast_result']; exec(open('$CRISPR_classification').read())"
+  python3 -c "import sys; sys.path.append('$python_module_dir'); from CRISPR_classification import CRISPR_classification; CRISPR_classification('${result_summary}/result_process.csv', '${result_summary}/cas', '${result_summary}/CRISPR_sort', '${result_summary}/blast_result')"
+
+  # python3 -c "import sys; sys.path.append('$python_module_dir'); from Spacer_ct_numbering import summary; summary()"
+  # python3 -c "import sys; sys.path.append('$python_module_dir'); sys.argv = ['Spacer_ct_numbering.py', '$result_summary/CRISPR_sort', '$result_summary/spacer/spacer_order', '$result_summary/spacer/serial', '$result_summary/spacer/ct']; exec(open('$CT_number').read())"
+  python3 -c "import sys; sys.path.append('$python_module_dir'); from Spacer_ct_numbering import summary; summary('${result_summary}/CRISPR_sort', '${result_summary}/CRISPR_typing/spacer_order', '${result_summary}/CRISPR_typing/serial', '${result_summary}/CRISPR_typing/ct')"
+
+  #--------------------------------------------------------------------------------
+  # mlst
+  output_mlst="$result_summary/mlst"
+
+  if [[ "$MLST" == true ]]; then
+      if ! command -v mlst &> /dev/null; then
+        echo -e "\033[1;31mError:\033[0m 'mlst' command not found. Please install mlst first."
+        exit 1
+      else
+        mkdir -p "$output_mlst"
+        # mlst --scheme cronobacter --legacy --csv *.fna > mlst_results.csv
+        mlst --scheme cronobacter --legacy --csv ${CSI}/*.fna > ${output_mlst}/mlst_results.csv
+      fi
+  fi
+
+  exit 0
+
+#================================================================================
+elif [[ "$PCR_seq" == true && "$CT" == true ]]; then
 
   bash "$script_dir/CRISPR.sh" "$CSI" "$CSO"
 
